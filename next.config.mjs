54147bf1d680a2994440303acb3a697e199c828f
re-roze <re2022.roze@gmail.com>
2025-04-02 12:10:46 +0900

    next mjs edited

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
-// next.config.js 또는 next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true, // ✅ 이 안에 있어야 함!
-    },
-  };
-  
-  module.exports = nextConfig;
+    eslint:{
+        ignoreDuringBuilds:true,
+    }
+};
+
+export default nextConfig;
